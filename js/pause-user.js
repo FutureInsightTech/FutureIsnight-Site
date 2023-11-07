cb546c11d50934c4b127bde531163b0fea966222
rafay99-epic <99marafay@gmail.com>
2023-11-07 10:29:16 +0500

    Version-07: Testing Search Feature Deploy

---
+++ js/pause-user.js
@@ -0,0 +1,13 @@
+// Author: Abdul Rafay
+// Script: This script will pause the user and show a message, this part in under development
+
+// Working of this Script: 👇
+
+// Calling this function buy using onclick="showUnderDevMessage()" in html and inport this scrip in your folder
+//  onclick="showUnderDevMessage()"
+
+// Pause user and show error message
+function showUnderDevMessage() {
+    event.preventDefault();
+   alert('This section is currently under development. Please check back later.');
+ }
